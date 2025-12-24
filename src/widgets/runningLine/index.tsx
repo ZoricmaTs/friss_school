0c4746c578a35acecbf2c436730872c3b232e5d9
zori <tszzzch@gmail.com>
2025-12-24 21:48:14 +0600

    fix running line, map

--- src/widgets/runningLine/index.tsx
+++ src/widgets/runningLine/index.tsx
@@ -2,6 +2,7 @@ import './style.scss';
 import {ScissorsIcon} from '@phosphor-icons/react';
 import {useScrollHider} from '../../hooks/scroll-observer.ts';
 import useSheetData from '../../hooks/useSheetData.ts';
+import {useScreen} from '../../hooks/useScreen.ts';
 
 export type LineType = {
   id: number,
@@ -11,6 +12,8 @@ export type LineType = {
 export function RunningLine() {
   const rootRef = useScrollHider<HTMLDivElement>();
   const data = useSheetData(1124678627) as unknown as LineType [];
+  const {width} = useScreen();
+  const iconSize: number = width < 1200 ? Math.floor(4 * width / 100) : 50;
 
   return <div className={'running-line'} ref={rootRef}>
     <div className={'running-line__inner'}>
@@ -19,14 +22,14 @@ export function RunningLine() {
       >
         {data.map((line, index: number) => {
           return <div className={'running-line__item'} key={`line-first_${index}`}>
-            <ScissorsIcon size={80}/>
-            <h1 style={{marginRight: '3rem', marginLeft: '3rem', fontWeight: 500}}>{line.text}</h1>
+            <ScissorsIcon size={iconSize}/>
+            <h3 className={'running-line__text'}>{line.text}</h3>
           </div>
         })}
         {data.map((line, index: number) => {
           return <div className={'running-line__item'} key={`line-second_${index}`}>
-            <ScissorsIcon size={80}/>
-            <h1 style={{marginRight: '3rem', marginLeft: '3rem', fontWeight: 500}}>{line.text}</h1>
+            <ScissorsIcon size={iconSize}/>
+            <h3 className={'running-line__text'}>{line.text}</h3>
           </div>
         })}
       </div>
