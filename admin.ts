0e499384640e365c3e3e6e7f6294f554253f9dd1
whiteGloom <karabin.m@yandex.ru>
2026-02-19 06:15:39 +0600

    feat: ready to try
    BranchName: emiller/feat/dynamic-data

--- admin.ts
+++ admin.ts
@@ -1,15 +1,10 @@
 import exec from 'node:child_process';
-import open from 'open';
 
 /// File starts two npm runs simultaneously and waits until some of them closes
 async function run() {
-  new Promise(resolve => setTimeout(resolve, 10000)).then(() => {
-    return open('http://localhost:3010/admin');
-  }).catch(null);
-
   return Promise.all([
     exec.exec('npm run admin-server'),
-    exec.exec('npm run dev'),
+    exec.exec('npm run dev-admin'),
   ]);
 }
 
