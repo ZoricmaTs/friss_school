155ecb087a7eca412ad0d202deb70916a14c6da3
whiteGloom <karabin.m@yandex.ru>
2026-02-19 06:02:42 +0600

    feat: ready to try
    BranchName: emiller/feat/dynamic-data

--- bootstrap/friss.ps1
+++ bootstrap/friss.ps1
@@ -4,34 +4,37 @@ $gitPath = "$scriptDir\PortableGit"
 $gitBinariesPath = "$gitPath\bin"
 
 if (Test-Path "$gitBinariesPath") {
-    Write-Host "Git установлен." -ForegroundColor Green
+    Write-Host "Git is installed." -ForegroundColor Green
 } else {
     $gitUrl = "https://github.com/git-for-windows/git/releases/download/v2.52.0.windows.1/PortableGit-2.52.0-64-bit.7z.exe"
     $zipName = "temp_git_archive.7z"
     $zipFile = "$scriptDir\$zipName"
     $exeFile = "$zipFile.exe"
 
-    Write-Host "Git не установлен." -ForegroundColor Red
+    Write-Host "Git isn't installed" -ForegroundColor Red
 
-    Write-Host "Скачиваем архив с Git..." -ForegroundColor Cyan
+    Write-Host "Downloading Git archive..." -ForegroundColor Cyan
     Invoke-WebRequest -Uri $gitUrl -OutFile $exeFile
 
     if (-not (Test-Path $gitPath)) {
         New-Item -Path $gitPath -ItemType Directory
     }
 
-    Write-Host "Распаковываем архив по пути: $gitPath..." -ForegroundColor Cyan
+    Write-Host "Unpacking Git to path: $gitPath..." -ForegroundColor Cyan
 
     if (Test-Path "C:\Program Files\7-Zip") {
+        Write-Host "7-Zip is installed! Unpacking silently..." -ForegroundColor Red
         Rename-Item -Path $exeFile -NewName $zipName
         & "C:\Program Files\7-Zip\7z.exe" x $zipFile "-o$gitPath" -y > $null
-        Remove-Item $zipFile!
-    } else { 
-        & $exeFile
+        Remove-Item $zipFile
+        Remove-Item $exeFile
+    } else {
+        Write-Host "7-Zip isn't installed! Follow installer instructions" -ForegroundColor Red
+        Start-Process $exeFile -Wait
         Remove-Item $exeFile
     }
 
-    Write-Host "Готово!" -ForegroundColor Green
+    Write-Host "Done!" -ForegroundColor Green
 }
 
 
@@ -41,20 +44,20 @@ $nodePath = "$scriptDir\node"
 $nodeBinariesPath = "$nodePath\inner"
 
 if (Test-Path $nodeBinariesPath) { 
-    Write-Host "NodeJs установлен." -ForegroundColor Green
+    Write-Host "NodeJs is installed." -ForegroundColor Green
 } else {
     $nodeUrl = "https://nodejs.org/dist/v24.12.0/node-v24.12.0-win-x64.zip"
     $zipFile = "$scriptDir\temp_node_archive.zip"
-    Write-Host "NodeJs не установлен." -ForegroundColor Red
+    Write-Host "NodeJs isn't installed." -ForegroundColor Red
 
-    Write-Host "Скачиваем архив с NodeJs..." -ForegroundColor Cyan
+    Write-Host "Downloading NodeJs archive..." -ForegroundColor Cyan
     Invoke-WebRequest -Uri $nodeUrl -OutFile $zipFile
 
     if (-not (Test-Path $nodePath)) {
         New-Item -Path $nodePath -ItemType Directory
     }
 
-    Write-Host "Распаковываем архив по пути: $nodePath..." -ForegroundColor Cyan
+    Write-Host "Unpacking node to path: $nodePath..." -ForegroundColor Cyan
     Expand-Archive $zipFile $nodePath -Force
 
     $innerNodeFolder = Get-ChildItem -Path $nodePath | Select-Object -First 1
@@ -62,7 +65,7 @@ if (Test-Path $nodeBinariesPath) {
 
     Remove-Item $zipFile
 
-    Write-Host "Готово!" -ForegroundColor Green
+    Write-Host "Done!" -ForegroundColor Green
 }
 
 $env:Path = "$nodeBinariesPath;" + $env:Path
